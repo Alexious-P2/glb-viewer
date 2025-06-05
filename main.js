29c8b6e8ec310293b0f7d914d63118bd3fafa16d
Alexious-P2 <alexious.fernando@position2.com>
2025-06-05 17:02:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -137,7 +137,7 @@ ambientFolder.open();
 
 // Ground Plane
 const groundGeo = new THREE.PlaneGeometry(20, 20);
-const groundMat = new THREE.ShadowMaterial({ opacity: 0.3, roughness: 0.1, metalness: 0.1 });
+const groundMat = new THREE.ShadowMaterial({ opacity: 0.3 });
 
 const ground = new THREE.Mesh(groundGeo, groundMat);
 ground.rotation.x = -Math.PI / 2;
