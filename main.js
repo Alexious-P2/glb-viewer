125750d04de8255aad6bc589111ef17b4d3ee961
Alexious-P2 <alexious.fernando@position2.com>
2025-06-10 15:19:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -242,7 +242,7 @@ const ssrPass = new SSRPass({
   width: window.innerWidth,
   height: window.innerHeight,
   groundReflector: groundReflector,
-  selects: [groundReflector] // null You can specify reflective meshes if you want 
+  selects: [groundReflector], [reflectiveMesh] // null You can specify reflective meshes if you want 
 });  
 
 ssrPass.maxDistance = 0.1;
